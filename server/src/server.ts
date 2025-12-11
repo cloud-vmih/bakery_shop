c944cd87a09ac4e0b46995f22214dc0f22cb1025
hgiang-workspace <giang812105@gmail.com>
2025-12-11 21:01:20 +0700

    Conditionally load dotenv configuration
    Load environment variables conditionally based on NODE_ENV.

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,7 +6,9 @@ import cartRoutes from "./routes/cart.routes";
 
 
 import dotenv from "dotenv";
-dotenv.config();
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config();
+}
 const app = express();
 
 app.use(cors({
