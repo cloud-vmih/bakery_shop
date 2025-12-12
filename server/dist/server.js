8a3cb85718b5b70692ca2766d4171b125a2c3343
hgiang-workspace <giang812105@gmail.com>
2025-12-12 14:49:01 +0700

    Remove trailing slash from frontend origin

--- server/dist/server.js
+++ server/dist/server.js
@@ -12,7 +12,7 @@ const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)({
-    origin: "https://bakery-taupe.vercel.app/", // chỉ định frontend origin
+    origin: "https://bakery-taupe.vercel.app", // chỉ định frontend origin
     credentials: true // cho phép gửi cookie / auth headers
 }));
 app.use(express_1.default.json());
